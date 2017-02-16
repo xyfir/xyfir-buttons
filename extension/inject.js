a6d81dd484ad78567a0260bbe55800a97b836c61
MrXyfir <mr@xyfir.com>
2017-02-16 09:59:44 -0800

    Add inject.js

---
+++ extension/inject.js
@@ -0,0 +1,14 @@
+import onUrlChange from 'lib/inject/events/on-url-change';
+import onMessage from 'lib/inject/events/on-message';
+import getPreset from 'lib/inject/get-preset';
+
+window._xyb_ = {
+  transactions: {}, buttons: {}, presets: {
+    current: {}, matches: []
+  }
+};
+
+window.addEventListener('message', onMessage);
+window.addEventListener('popstate', onUrlChange);
+
+getPreset();
