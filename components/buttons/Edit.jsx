9172e8061978788d4a7ed74b3a3c8a2dcd42308d
MrXyfir <mr@xyfir.com>
2017-02-18 14:59:51 -0800

    Add EditButton component

---
+++ components/buttons/Edit.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import request from 'superagent';
+
+// react-md
+import Paper from 'react-md/lib/Papers';
+
+// Components
+import Form from 'components/buttons/Form';
+
+// Constants
+import { XYBUTTONS_URL } from 'constants/config';
+
+class EditButton extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    const button = this.props.App.state.storage[
+      'button_' + this.props.params.button
+    ];
+
+    if (!button) location.hash = '#/buttons';
+
+    this.state = { button };
+  }
+
+  /**
+   * Call xyButtons API to edit the button.
+   * @param {object} button - The button object passed from
+   * CreateOrEditButtonForm's onValidate().
+   */
+  onEdit(button) {
+    const id = this.state.button.id;
+
+    request
+      .put(XYBUTTONS_URL + 'api/buttons/' + id)
+      .send(button)
+      .end((err, res) => {
+        if (err || res.body.error)
+          this.props.App.onAlert(res.body.message);
+        else
+          chrome.storage.set({ ['button_' + id]: button });
+      });
+  }
+
+  render() {
+    return (
+      <Paper zDepth={1} className='create-button'>
+        <Form onSuccess={this.onCreate} button={this.state.button} />
+      </Paper>
+    );
+  }
+
+}
+
+export default EditButton;
