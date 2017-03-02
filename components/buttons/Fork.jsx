5a8012460b6cde05a9f5d49af1f066e81b96f9a2
MrXyfir <mr@xyfir.com>
2017-03-02 15:57:26 -0800

    Use this.state.isCreator

--- components/buttons/Fork.jsx
+++ components/buttons/Fork.jsx
@@ -35,7 +35,10 @@ export default class ForkButton extends React.Component {
         else {
           this.setState({
             loading: false,
-            isCreator: this.props.storage.account.uid == res.body.creator.id
+            isCreator: (
+              res.body.creator.id == this.props.storage.account.uid
+              && res.body.creator.id != 0
+            )
           });
         }
       });
@@ -66,7 +69,7 @@ export default class ForkButton extends React.Component {
     return (
       <Tabs
         id={+this.props.params.button}
-        isCreator={true}
+        isCreator={this.state.isCreator}
         activeTabIndex={1}
       >
         <Paper zDepth={1} className='fork-button'>
