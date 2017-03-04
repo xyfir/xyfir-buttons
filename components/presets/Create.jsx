a76205318a6c914de630be41b7bb65e7e86d5313
MrXyfir <mr@xyfir.com>
2017-03-04 15:44:57 -0800

    Add CreatePreset component

---
+++ components/presets/Create.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import request from 'superagent';
+
+// react-md
+import Paper from 'react-md/lib/Papers';
+
+// Components
+import Form from 'components/presets/Form';
+
+// Constants
+import { XYBUTTONS_URL } from 'constants/config';
+
+// Modules
+import downloadPresets from 'lib/app/presets/download';
+
+class CreatePreset extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  /**
+   * Call xyButtons API to create preset.
+   * @param {object} preset - The preset object passed from
+   * CreateOrEditPresetForm's onValidate().
+   */
+  onCreate(preset) {
+    request
+      .post(XYBUTTONS_URL + 'api/presets')
+      .send(preset)
+      .end((err, res) => {
+        if (err || res.body.error) {
+          this.props.App._alert(res.body.message);
+        }
+        else {
+          const next = () => location.hash = '#/presets/' + res.body.id;
+          downloadPresets([{ id: res.body.id }]).then(next).catch(next);
+        }
+      });
+  }
+
+  render() {
+    return (
+      <Paper zDepth={1} className='create-preset'>
+        <Form {...this.props} onSuccess={b => this.onCreate(b)} />
+      </Paper>
+    );
+  }
+
+}
+
+export default CreatePreset;
