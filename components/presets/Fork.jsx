f2cc3023dce02fbd0550375af02f460f5f50bad8
MrXyfir <mr@xyfir.com>
2017-03-04 15:48:11 -0800

    Add ForkPreset component

---
+++ components/presets/Fork.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import request from 'superagent';
+
+// react-md
+import Paper from 'react-md/lib/Papers';
+import Button from 'react-md/lib/Buttons/Button';
+
+// Components
+import Tabs from 'components/misc/Tabs';
+
+// Constants
+import { XYBUTTONS_URL } from 'constants/config';
+
+// Modules
+import downloadPresets from 'lib/app/presets/download';
+
+export default class ForkPreset extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+
+  /**
+   * Check if button exists and set state.isCreator.
+   */
+  componentWillMount() {
+    request
+      .get(`${XYBUTTONS_URL}api/presets/${this.props.params.preset}`)
+      .end((err, res) => {
+        if (err || res.body.id == -1) {
+          location.hash = '#/presets';
+        }
+        else {
+          this.setState({
+            loading: false,
+            isCreator: (
+              res.body.creator.id == this.props.storage.account.uid
+              && res.body.creator.id != 0
+            )
+          });
+        }
+      });
+  }
+
+  /**
+   * Create a fork of the preset and download the copy and its buttons.
+   */
+  onFork() {
+    const id = this.props.params.preset;
+
+    request
+      .post(`${XYBUTTONS_URL}api/presets/${id}/fork`)
+      .end((err, res) => {
+        if (err || res.body.error) {
+          this.props.App._alert('Could not fork preset');
+        }
+        else {
+          const next = () => location.hash = '#/presets/' + res.body.id;
+          downloadPresets([{ id: res.body.id }]).then(next).catch(next);
+        }
+      });
+  }
+
+  render() {
+    if (this.state.loading) return <div />;
+
+    return (
+      <Tabs
+        base={'#/presets/' + this.props.params.preset}
+        isCreator={this.state.isCreator}
+        activeTabIndex={1}
+      >
+        <Paper zDepth={1} className='fork-preset'>
+          <p>
+            Are you sure you want to fork this preset?
+            <br />
+            A copy of this preset will be created under your account. Only the preset's data, and not its download stats, comments, votes, etc will be copied.
+          </p>
+          <Button
+            raised primary
+            label='Fork Preset'
+            onClick={() => this.onFork()}
+          >call_split</Button>
+        </Paper>
+      </Tabs>
+    );
+  }
+
+}
