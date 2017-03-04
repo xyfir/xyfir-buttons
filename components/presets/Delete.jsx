b006194f8aef9ad2e7f84dccf187ce70946fb0e5
MrXyfir <mr@xyfir.com>
2017-03-04 15:46:54 -0800

    Add DeletePreset component

---
+++ components/presets/Delete.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import request from 'superagent';
+
+// react-md
+import Paper from 'react-md/lib/Papers';
+import Button from 'react-md/lib/Buttons/Button';
+
+// Components
+import Tabs from 'components/misc/Tabs';
+
+// Constants
+import { XYBUTTONS_URL } from 'constants/config';
+
+export default class DeletePreset extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  /**
+   * Attempt to delete preset via API. If successful it's also deleted from
+   * local storage.
+   */
+  onDelete() {
+    const id = this.props.params.preset;
+
+    request
+      .delete(`${XYBUTTONS_URL}api/presets/${id}`)
+      .end((err, res) => {
+        if (err || res.body.error) {
+          this.props.App._alert('Could not delete preset');
+        }
+        else {
+          chrome.storage.local.remove(
+            'preset_' + id, () => location.hash = '#/presets'
+          );
+        }
+      });
+  }
+
+  render() {
+    return (
+      <Tabs
+        base={'#/presets/' + this.props.params.preset}
+        isCreator={true}
+        activeTabIndex={3}
+      >
+        <Paper zDepth={1} className='delete-preset'>
+          <p>
+            Are you sure you want to delete this preset?
+            <br />
+            It will be deleted both from your local system and from Xyfir Buttons' servers. Forks of it will remain untouched.
+          </p>
+          <Button
+            raised primary
+            label='Delete Preset'
+            onClick={() => this.onDelete()}
+          >delete</Button>
+        </Paper>
+      </Tabs>
+    );
+  }
+
+}
