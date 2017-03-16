aeaa6f4f0080af16554748dea044665ce656c5c2
MrXyfir <mr@xyfir.com>
2017-03-16 12:41:42 -0700

    Use lib/background/modify-csp

--- main/background.js
+++ main/background.js
@@ -1,6 +1,7 @@
 import onStorageChange from 'lib/background/on-storage-change';
 import chromePromise from 'chrome-promise';
 import onMessage from 'lib/background/on-message';
+import modifyCSP from 'lib/background/modify-csp';
 import onStart from 'lib/background/on-start';
 
 chrome.p = new chromePromise();
@@ -10,5 +11,12 @@ chrome.storage.onChanged.addListener(onStorageChange);
 
 // Listen for events from content-script.js
 chrome.runtime.onMessage.addListener(onMessage);
+
+// Modifies content security policy to prevent the blocking of eval()
+chrome.webRequest.onHeadersReceived.addListener(
+  modifyCSP,
+  {urls: ['*://*/*'], types: ['main_frame']},
+  ['blocking', 'responseHeaders']
+);
 
 onStart();
