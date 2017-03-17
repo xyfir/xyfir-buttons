10a76cd3853978923944949170da7f0c91d9203e
MrXyfir <mr@xyfir.com>
2017-03-17 14:54:05 -0700

    Add lib/content-script/login

--- main/content-script.js
+++ main/content-script.js
@@ -2,10 +2,12 @@ import chromePromise from 'chrome-promise';
 import injectFiles from 'lib/content-script/inject-files';
 import onMessage from 'lib/content-script/on-message';
 import getTabId from 'lib/content-script/get-tab-id';
+import login from 'lib/content-script/login';
 
 chrome.p = new chromePromise();
 
 window.addEventListener('message', onMessage);
 
+login();
 getTabId();
 injectFiles();
