16a7d949f6f54684362e7b2e90b1b094078314d7
MrXyfir <mr@xyfir.com>
2017-03-28 17:24:16 -0700

    Change default values

--- constants/config.default.js
+++ constants/config.default.js
@@ -1,4 +1,4 @@
-export const XYBUTTONS_URL = '';
-export const ENVIRONMENT = '';
+export const XYBUTTONS_URL = 'https://buttons.xyfir.com/';
+export const ENVIRONMENT = 'dev'; // dev|prod
 export const STRIPE_KEY = '';
-export const XYDOCS_URL = '';
+export const XYDOCS_URL = 'https://api.github.com/repos/Xyfir/Documentation/contents/';
