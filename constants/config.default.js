e97575efff1ecf58589404142811adbec8084634
MrXyfir <mr@xyfir.com>
2017-04-08 19:53:45 -0700

    Add XYACCOUNTS_URL

--- constants/config.default.js
+++ constants/config.default.js
@@ -1,3 +1,4 @@
+export const XYACCOUNTS_URL = 'https://accounts.xyfir.com/';
 export const XYBUTTONS_URL = 'https://buttons.xyfir.com/';
 export const ENVIRONMENT = 'dev'; // dev|prod
 export const STRIPE_KEY = '';
