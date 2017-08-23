087a07ccbab4a301a37d9e5ce191a88454bf453f
MrXyfir <mr@xyfir.com>
2017-08-23 16:58:59 -0700

    Remove sync, users, login

--- main/background.js
+++ main/background.js
@@ -1,13 +1,9 @@
-import onStorageChange from 'lib/background/on-storage-change';
 import chromePromise from 'chrome-promise';
+import wipeTempData from 'lib/background/wipe-temp-data';
 import onMessage from 'lib/background/on-message';
 import modifyCSP from 'lib/background/modify-csp';
-import onStart from 'lib/background/on-start';
 
 chrome.p = new chromePromise();
-
-// Listen for changes made to storage by app.js or content-script.js
-chrome.storage.onChanged.addListener(onStorageChange);
 
 // Listen for events from content-script.js
 chrome.runtime.onMessage.addListener(onMessage);
@@ -19,4 +15,4 @@ chrome.webRequest.onHeadersReceived.addListener(
   ['blocking', 'responseHeaders']
 );
 
-onStart();
+wipeTempData();
--- main/content-script.js
+++ main/content-script.js
@@ -2,12 +2,10 @@ import chromePromise from 'chrome-promise';
 import injectFiles from 'lib/content-script/inject-files';
 import onMessage from 'lib/content-script/on-message';
 import getTabId from 'lib/content-script/get-tab-id';
-import login from 'lib/content-script/login';
 
 chrome.p = new chromePromise();
 
 window.addEventListener('message', onMessage);
 
-login();
 getTabId();
 injectFiles();
