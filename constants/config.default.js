ee5eea7fb5e5f9bdedd79a0618c13cabf679d7a6
MrXyfir <mr@xyfir.com>
2017-08-23 17:01:22 -0700

    Remove unused keys

--- constants/config.default.js
+++ constants/config.default.js
@@ -1,5 +1,1 @@
-export const XYACCOUNTS_URL = 'https://accounts.xyfir.com/';
-export const XYBUTTONS_URL = 'https://buttons.xyfir.com/';
-export const ENVIRONMENT = 'dev'; // dev|prod
-export const STRIPE_KEY = '';
 export const XYDOCS_URL = 'https://api.github.com/repos/Xyfir/Documentation/contents/';
